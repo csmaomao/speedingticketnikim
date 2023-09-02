817068d053f1e7b9f3f85120230c1c5992ba7b9a
ck <143800772+csmaomao@users.noreply.github.com>
2023-09-02 09:08:27 +0800

    Add files via upload

---
+++ SpeedingTicketNiKim/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//simple speedint ticket program
+//assume anything going over 120 is speeding
+Console.WriteLine("Provide Speed: ");
+double speed = Double.Parse(Console.ReadLine());
+
+if (speed > 120)
+{
+    Console.WriteLine("Speeding ticket!");
+}
+else
+{
+    Console.WriteLine("Carry on ahead");
+}
+Console.ReadLine();
+
+
